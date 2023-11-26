f0d7fb7f85fe3e2b57d85f24eda90af48189b7a6
csesopnil <sopnilstar@gmail.com>
2023-11-26 15:12:11 +0600

    dgdgd

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
     "./node_modules/tw-elements/dist/js/**/*.js",
     "./node_modules/flowbite/**/*.js",
+    "./node_modules/tw-elements-react/dist/js/**/*.js"
 
   ],
   theme: {
@@ -13,6 +14,7 @@ export default {
   plugins: [
     require("tw-elements/dist/plugin.cjs"),
     require('flowbite/plugin'),
+    require("tw-elements-react/dist/plugin.cjs")
   ],
   darkMode: "class"
 }
