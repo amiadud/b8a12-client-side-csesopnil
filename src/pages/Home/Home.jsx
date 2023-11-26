3866d7ce2e8a892152b464820202a39ce4ebad56
csesopnil <sopnilstar@gmail.com>
2023-11-26 15:24:14 +0600

    added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import Banner from '../../shared/Banner/Banner';
 import PetCategory from '../../components/PetCategory/PetCategory';
-import Banner2 from '../../shared/Banner2/Banner2';
-import AboutUs from '../../shared/Aboutus/AboutUs';
+
 
 const Home = () => {
     return (
         <div>
             <Banner/>
             <PetCategory/>
-            <Banner2/>
             {/* <AboutUs/> */}
         </div>
     );
