1ee6838419686cb07a6a1e95b2d6aa0cbe5f7a91
csesopnil <sopnilstar@gmail.com>
2023-11-28 12:58:05 +0600

    insert update page redirection added

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -11,7 +11,7 @@ const Footer = () => {
     </span>
     <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
         <li>
-            <a href="#" className="hover:underline me-4 md:me-6">Abou</a>
+            <a href="#" className="hover:underline me-4 md:me-6">About</a>
         </li>
         <li>
             <a href="#" className="hover:underline me-4 md:me-6">Privacy Policy</a>
